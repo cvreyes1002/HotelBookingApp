0988d687b29daad079bb6ea824dee7707dd21c3a
C.Reyes <cvreyes@hotmail.com>
2026-08-18 15:48:46 +0800

    finish Home page. started rooms page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { AuthProvider } from "./components/AuthProvider";
+import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 const Logout = () => {
   localStorage.clear();
@@ -19,10 +21,12 @@ const App = () => {
         <div className="min-h-[70vh]">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<AllRooms />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
+        <Footer />
       </AuthProvider>
     </div>
   );
