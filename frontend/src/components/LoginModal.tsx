f607d06cf8f74948fb7d76c31ad4ee620f7e6a6d
C.Reyes <cvreyes@hotmail.com>
2026-08-18 21:11:45 +0800

    change login modal to include registration

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -11,6 +11,13 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
   const { login } = useAuth();
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [state, setState] = useState("login");
+
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   email: "",
+  //   password: "",
+  // });
 
   if (!isOpen) return null; // If the modal isn't open, render nothing
 
@@ -20,31 +27,35 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
     setErrorMessage(null);
 
     const target = e.currentTarget;
-    const email = (target.elements.namedItem("email") as HTMLInputElement)
-      .value;
-    const password = (target.elements.namedItem("password") as HTMLInputElement)
-      .value;
+    const email = (target.elements.namedItem("email") as HTMLInputElement).value;
+    const password = (target.elements.namedItem("password") as HTMLInputElement).value;
 
-    // const email = e.target.email.value;
-    // const password = e.target.password.value;
-
-    try {
-      await login(email, password);
-      onClose(); // Close the modal after successful login
-    } catch (err: any) {
-      const message = err.message || "Login failed";
-      setErrorMessage(message); // Captures the error thrown by AuthProvider
-      console.log(message);
-    } finally {
-      setLoading(false);
+    if (state === "register") {
+      const name = (target.elements.namedItem("email") as HTMLInputElement).value;
+    } else {
+        try {
+        await login(email, password);
+        onClose(); // Close the modal after successful login
+      } catch (err: any) {
+        const message = err.message || "Login failed";
+        setErrorMessage(message); // Captures the error thrown by AuthProvider
+        console.log(message);
+      } finally {
+        setLoading(false);
+      }
     }
+  };
+   
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
     <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-100">
       <form
         onSubmit={handleSubmit}
-        className="relative bg-white text-gray-500 max-w-87.5 w-full mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10"
+        className="relative sm:w-87.5 w-full text-center bg-gray-900 border border-gray-800 rounded-2xl px-8"
       >
         {/* Close Button */}
         <button
@@ -69,49 +80,125 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
           </svg>
         </button>
 
-        <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
-          Login Now
-        </h2>
+        <h1 className="text-white text-3xl mt-10 font-medium">
+          {state === "login" ? "Login" : "Sign up"}
+        </h1>
+
+        <p className="text-gray-400 text-sm mt-2">Please sign in to continue</p>
 
         {loading && <LoadingIndicator />}
 
-        <input
-          id="email"
-          name="email"
-          className="w-full border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
-          type="email"
-          placeholder="Enter your email"
-          required
-        />
-        <input
-          id="password"
-          name="password"
-          className="w-full border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
-          type="password"
-          placeholder="Enter your password"
-          required
-        />
-
-        {errorMessage && (
-          <p className="text-red-500 text-center mb-3">{errorMessage}</p>
+        {state !== "login" && (
+          <div className="flex items-center mt-6 w-full bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              className="text-gray-400"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              {" "}
+              <circle cx="12" cy="8" r="5" />{" "}
+              <path d="M20 21a8 8 0 0 0-16 0" />{" "}
+            </svg>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none "
+              // value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
         )}
 
-        <div className="text-right py-4">
-          <a className="text-blue-600 underline" href="#">
-            Forgot Password
-          </a>
+        <div className="flex items-center w-full mt-4 bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            className="text-gray-400"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            {" "}
+            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />{" "}
+            <rect x="2" y="4" width="20" height="16" rx="2" />{" "}
+          </svg>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email id"
+            className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none "
+            // value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
+
+        <div className=" flex items-center mt-4 w-full bg-gray-800 border border-gray-700 h-12 rounded-full overflow-hidden pl-6 gap-2 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            className="text-gray-400"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            {" "}
+            <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />{" "}
+            <path d="M7 11V7a5 5 0 0 1 10 0v4" />{" "}
+          </svg>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full bg-transparent text-white placeholder-gray-400 border-none outline-none"
+            // value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="mt-4 text-left">
+          <button className="text-sm text-indigo-400 hover:underline">
+            Forget password?
+          </button>
+        </div>
+
         <button
           type="submit"
-          className="w-full mb-3 bg-indigo-500 hover:bg-indigo-600/90 active:scale-95 transition py-2.5 rounded-full text-white"
+          className="mt-2 w-full h-11 rounded-full text-white bg-indigo-600 hover:bg-indigo-500 transition "
         >
-          Log in
+          {state === "login" ? "Login" : "Sign up"}
         </button>
-        <p className="text-center mt-4">
-          Don’t have an account?{" "}
-          <a href="#" className="text-blue-500 underline">
-            Signup Now
-          </a>
+
+        <p
+          onClick={() =>
+            setState((prev) => (prev === "login" ? "register" : "login"))
+          }
+          className="text-gray-400 text-sm mt-3 mb-11 cursor-pointer"
+        >
+          {state === "login"
+            ? "Don't have an account?"
+            : "Already have an account?"}
+          <span className="text-indigo-400 hover:underline ml-1">
+            click here
+          </span>
         </p>
       </form>
     </div>
